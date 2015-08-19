f91967781916300a8976c431675dee83ea61db9d
Jan Wroblewski <e55951@jp.ibm.com>
2015-08-19 16:47:52 +0900

    Fixed run-tests to work with IBM JDK version naming.

--- dev/run-tests.py
+++ dev/run-tests.py
@@ -164,7 +164,13 @@ def determine_java_version(java_exe):
     # find raw version string, eg 'java version "1.8.0_25"'
     raw_version_str = next(x for x in raw_output_lines if " version " in x)
 
-    match = re.search('(\d+)\.(\d+)\.(\d+)_(\d+)', raw_version_str)
+    version_str = raw_version_str.split()[-1].strip('"')  # eg '1.8.0_25'
+    verup = version_str.split('_')  # eg ['1.8.0', '25'] for OpenJDK or just ['1.8.0'] for IBM jdk
+    version = verup[0]
+    update = verup[1] if len(verup) > 1 else 0
+
+    # map over the values and convert them to integers
+    version_info = [int(x) for x in version.split('.') + [update]]
 
     major = int(match.group(1))
     minor = int(match.group(2))
