51f316850fb30185bdf567f0d1827fef1940b549
Jan Wroblewski <e55951@jp.ibm.com>
2015-08-20 13:30:07 +0900

    Moved utility scripts outside of the repository.

--- core/test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mvn -Pyarn -Phadoop-2.3 -Dhadoop.version=2.3.0 -Phive -Pkinesis-asl -Phive-thriftserver --fail-at-end test -DwildcardSuites=org.apache.spark.ColumnPartitionSchema,org.apache.spark.storage.BlockManagerSuite,org.apache.spark.CacheManagerSuite -Dtest=none
