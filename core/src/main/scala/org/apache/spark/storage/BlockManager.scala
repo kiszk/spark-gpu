0720623e31352a68c35a2f130589f1144a06669f
Jan Wroblewski <e55951@jp.ibm.com>
2015-08-28 16:49:11 +0900

    Added explicit evaluation in one point.

--- core/src/main/scala/org/apache/spark/storage/BlockManager.scala
+++ core/src/main/scala/org/apache/spark/storage/BlockManager.scala
@@ -1068,7 +1068,8 @@ private[spark] class BlockManager(
         // Drop to disk, if storage level requires
         if (level.useDisk && !diskStore.contains(blockId)) {
           logInfo(s"Writing block $blockId to disk")
-          data() match {
+          val dataVal = data()
+          dataVal match {
             case arr: Array[Any] =>
               diskStore.putArray(blockId, arr, level, returnValues = false)
             case cp: ColumnPartitionData[Any] =>
