18a815ed6ca78aab47a63dd40a0471efb92bd087
Jan Wroblewski <e55951@jp.ibm.com>
2015-08-31 18:22:30 +0900

    Fixed problem with wrongly named and imported PPCIBMJDKFailingTest tag.

--- core/src/test/scala/org/apache/spark/SparkTestTags.scala
+++ core/src/test/scala/org/apache/spark/SparkTestTags.scala
@@ -2,6 +2,6 @@ package org.apache.spark
 
 import org.scalatest.Tag
 
-private[spark] object SlowTest extends Tag("org.apache.sparktest.SlowTest")
-private[spark] object GPUTest extends Tag("org.apache.sparktest.GPUTest")
-private[spark] object PPCIBMJDKFailingTest extends Tag("org.apache.sparktest.PPCIBMJDKFailingTest")
+private[spark] object SlowTest extends Tag("org.apache.spark.SlowTest")
+private[spark] object GPUTest extends Tag("org.apache.spark.GPUTest")
+private[spark] object PPCIBMJDKFailingTest extends Tag("org.apache.spark.PPCIBMJDKFailingTest")
