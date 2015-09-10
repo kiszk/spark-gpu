b6191f303de5f5096728c9a6ac7b4d6957429d3c
Jan Wroblewski <e55951@jp.ibm.com>
2015-09-10 12:58:28 +0900

    Fixed weird Spark tests which SET the os.arch System property, breaking loading of native libs in subsequent tests.

--- gpudev/util/args.sh
+++ gpudev/util/args.sh
@@ -1,8 +1,8 @@
 DIR=../..
-export MAVEN_OPTS="$MAVEN_OPTS -Xmx32G -XX:MaxPermSize=8G -XX:ReservedCodeCacheSize=2G -Dos.arch=ppc64le"
+# might need to use -Dos.arch=ppc64le on OpenJDK in MAVEN_OPTS and JAVA_OPTS because of a bug
+export MAVEN_OPTS="$MAVEN_OPTS -Xmx32G -XX:MaxPermSize=8G -XX:ReservedCodeCacheSize=2G"
 MVN_ARGS="-Pyarn -Phadoop-2.4 -Dhadoop.version=2.4.0 -Pkinesis-asl -Phive-thriftserver -Phive"
-export JAVA_TOOL_OPTIONS="-Dos.arch=ppc64le"
-export JAVA_OPTS="-Xmx32G -XX:MaxPermSize=8G -XX:ReservedCodeCacheSize=2G -Dos.arch=ppc64le"
+export JAVA_OPTS="-Xmx32G -XX:MaxPermSize=8G -XX:ReservedCodeCacheSize=2G"
 
 #export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-ppc64el"
 export JAVA_HOME="/opt/ibm/ibm-java-ppc64le-80SR1FP10"
