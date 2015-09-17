38159bc4afc510aee52eb23ea253f4c3fc157faa
Jan Wroblewski <e55951@jp.ibm.com>
2015-09-17 12:04:12 +0900

    Made BlockManager column tests have correct os.arch to load the JCuda library properly.

--- core/src/test/scala/org/apache/spark/storage/BlockManagerSuite.scala
+++ core/src/test/scala/org/apache/spark/storage/BlockManagerSuite.scala
@@ -1333,6 +1333,9 @@ class BlockManagerSuite extends SparkFunSuite with Matchers with BeforeAndAfterE
   }
 
   test("serialize primitives - ColumnPartitionData[Int]") {
+    // This test loads external library, so it needs correct architecture
+    System.setProperty("os.arch", arch)
+
     sc = new SparkContext("local", "test", conf)
     assert(SparkEnv.get != null)
     assert(SparkEnv.get.executorMemoryManager != null)
@@ -1353,6 +1356,9 @@ class BlockManagerSuite extends SparkFunSuite with Matchers with BeforeAndAfterE
   }
 
   test("serialize case classes - ColumnPartitionData[Rectangle]") {
+    // This test loads external library, so it needs correct architecture
+    System.setProperty("os.arch", arch)
+
     sc = new SparkContext("local", "test", conf)
     store = makeBlockManager(10000)
     val blockId =  BlockId("rdd_42_42")
