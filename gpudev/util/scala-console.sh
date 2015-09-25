4cc71b7bd081c3efcd4aeaa1007a25132ade8567
Jan Wroblewski <e55951@jp.ibm.com>
2015-09-25 23:20:55 +0900

    Upgraded scala console dev util.

--- gpudev/util/scala-console.sh
+++ gpudev/util/scala-console.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source args.sh
 cd $DIR/core
-while true; do
-    ../$MVN_CMD $MVN_ARGS scala:console
-done
+trap '' 2
+export LD_LIBRARY_PATH="target/lib:$LD_LIBRARY_PATH"
+../$MVN_CMD $MVN_ARGS scala:console
