b7c9251fc8c890e48748880866f51865434da543
Jan Wroblewski <e55951@jp.ibm.com>
2015-09-27 18:56:40 +0900

    Updated namings of tests in dev tools.

--- gpudev/util/testdev.sh
+++ gpudev/util/testdev.sh
@@ -6,13 +6,13 @@ source args.sh
 cd $DIR
 
 SCALA_TESTS=\
-org.apache.spark.IteratedPartitionDataSuite,\
+org.apache.spark.IteratorPartitionDataSuite,\
 org.apache.spark.ColumnPartitionSchemaSuite,\
 org.apache.spark.ColumnPartitionDataSuite,\
 org.apache.spark.ColumnPartitionDataBuilderSuite,\
 org.apache.spark.cuda.CUDAManagerSuite,\
 org.apache.spark.cuda.CUDAKernelSuite,\
-org.apache.spark.ConversionRDDSuite,\
+org.apache.spark.ConvertRDDSuite,\
 org.apache.spark.storage.BlockManagerSuite,\
 org.apache.spark.rdd.RDDSuite,\
 org.apache.spark.CacheManagerSuite
