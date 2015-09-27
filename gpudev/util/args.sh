1138874eb304eea60e0ca6d97f2efcbf8254eedc
Jan Wroblewski <e55951@jp.ibm.com>
2015-09-27 21:24:38 +0900

    Updated dev tools to export native libraries in LD_LIBRARY_PATH.

--- gpudev/util/args.sh
+++ gpudev/util/args.sh
@@ -1,4 +1,5 @@
 DIR=../..
+ABSOLUTE_DIR=`realpath $DIR`
 # might need to use -Dos.arch=ppc64le on OpenJDK in MAVEN_OPTS and JAVA_OPTS because of a bug
 export MAVEN_OPTS="$MAVEN_OPTS -Xmx32G -XX:MaxPermSize=8G -XX:ReservedCodeCacheSize=2G"
 MVN_ARGS="-Pyarn -Phadoop-2.4 -Dhadoop.version=2.4.0 -Pkinesis-asl -Phive-thriftserver -Phive"
@@ -19,6 +20,8 @@ MVN_CMD="./build/mvn --force"
 DBG_PORT=5004
 
 export ZINC_PORT=3032
+
+export LD_LIBRARY_PATH="$ABSOLUTE_DIR/core/target/lib:$LD_LIBRARY_PATH"
 
 function killZinc() {
     kill `ps aux | grep zinc | grep java | grep $ZINC_PORT | awk '{print $2}'` &&
