80e3e884b6e760712691577f1e78e58de761adf8
Jan Wroblewski <e55951@jp.ibm.com>
2015-09-29 22:31:22 +0900

    Extended integration test.

--- gpudev/SparkGPUIntegrationTest/src/cuda/kernel.cu
+++ gpudev/SparkGPUIntegrationTest/src/cuda/kernel.cu
@@ -8,7 +8,7 @@ __global__ void multiplyBy2(int *in, int *out, long size) {
 
 __global__ void sum(int *input, int *output, long size, int stage, int totalStages) {
     const long ix = threadIdx.x + blockIdx.x * (long)blockDim.x;
-    const long jump = 4 * 32;
+    const int jump = 4 * 32;
     if (stage == 0) {
         int result = 0;
         for (long i = ix; i < size; i += jump) {
@@ -17,7 +17,7 @@ __global__ void sum(int *input, int *output, long size, int stage, int totalStag
         input[ix] = result;
     } else if (ix == 0) {
         int result = 0;
-        for (long i = 0; i < (size + jump - 1) / jump; ++i) {
+        for (long i = 0; i < jump; ++i) {
             result += input[i];
         }
         output[0] = result;
--- gpudev/SparkGPUIntegrationTest/src/main/scala/SparkGPUIntegrationTest.scala
+++ gpudev/SparkGPUIntegrationTest/src/main/scala/SparkGPUIntegrationTest.scala
@@ -3,12 +3,18 @@ import org.apache.commons.io.IOUtils
 import org.apache.spark.{SparkConf, SparkContext, ColumnFormat, IteratorFormat}
 import org.apache.spark.SparkContext._
 
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+
 object SparkGPUIntegrationTest {
 
   def main(args: Array[String]) {
     val conf = new SparkConf
     conf.setAppName("SparkGPUIntegrationTest")
     val sc = new SparkContext(conf)
+
+    Logger.getLogger("org.apache.spark").setLevel(Level.DEBUG);
+    Logger.getLogger("org.eclipse.jetty.server").setLevel(Level.OFF);
 
     var ok = true
 
@@ -23,6 +29,7 @@ object SparkGPUIntegrationTest {
       Array("this"),
       kernelData)
 
+    val stages = (size: Long) => 2
     val dimensions = (size: Long, stage: Int) => stage match {
       case 0 => (4, 32)
       case 1 => (1, 1)
@@ -34,34 +41,71 @@ object SparkGPUIntegrationTest {
       Array("this"),
       kernelData,
       Seq[AnyVal](),
-      Some(2),
+      Some(stages),
       Some(dimensions))
 
-    println("=== TEST 1 ===")
-    println("= map =")
-    val data = sc.parallelize(1 to 100000, 10)
+    {
+      println("=== TEST 1 ===")
+      println("= map =")
+      val n = 100000
+      val data = sc.parallelize(1 to n, 10)
+        .convert(ColumnFormat)
+        .mapUsingKernel((x: Int) => 2 * x, "multiplyBy2")
+        .collect()
+      println("Got data of length " + data.size)
+      print(data.take(5).mkString(", ") + " ... ")
+      println(data.takeRight(5).mkString(", "))
+      if (!data.sameElements((1 to n).map(_ * 2))) {
+        println("GOT WRONG DATA")
+        ok = false
+      }
+    }
+
+    {
+      println("=== TEST 2 ===")
+      println("= reduce =")
+      val n = 10000
+      val result = sc.parallelize(1 to n, 10)
+        .convert(ColumnFormat)
+        .reduceUsingKernel((x: Int, y: Int) => x + y, "sum")
+      println(s"Got result: $result")
+      if (result != n * (n + 1) / 2) {
+        println("GOT WRONG DATA")
+        ok = false
+      }
+    }
+
+    {
+    println("=== TEST 3 ===")
+    println("= map + map =")
+    val n = 100000
+    val data = sc.parallelize(1 to n, 10)
       .convert(ColumnFormat)
       .mapUsingKernel((x: Int) => 2 * x, "multiplyBy2")
-      .convert(IteratorFormat)
+      .mapUsingKernel((x: Int) => 2 * x, "multiplyBy2")
       .collect()
     println("Got data of length " + data.size)
     print(data.take(5).mkString(", ") + " ... ")
     println(data.takeRight(5).mkString(", "))
-    if (!data.sameElements((1 to 100000).map(_ * 2))) {
+    if (!data.sameElements((1 to n).map(_ * 4))) {
       println("GOT WRONG DATA")
       ok = false
     }
+    }
 
-    println("=== TEST 2 ===")
-    println("= map + reduce =")
-    val sum = sc.parallelize(1 to 10)
-      .convert(ColumnFormat)
-      .mapUsingKernel((x: Int) => 2 * x, "multiplyBy2")
-      .reduceUsingKernel((x: Int, y: Int) => x + y, "sum")
-    println(s"Got result: $sum")
-    if (sum != 100010000) {
-      println("GOT WRONG DATA")
-      ok = false
+    {
+      println("=== TEST 3 ===")
+      println("= map + reduce =")
+      val n = 10000
+      val result = sc.parallelize(1 to n, 10)
+        .convert(ColumnFormat)
+        .mapUsingKernel((x: Int) => 2 * x, "multiplyBy2")
+        .reduceUsingKernel((x: Int, y: Int) => x + y, "sum")
+      println(s"Got result: $result")
+      if (result != n * (n + 1)) {
+        println("GOT WRONG DATA")
+        ok = false
+      }
     }
 
     sc.stop()
