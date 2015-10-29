79e2cbef235f5d6b65ff51807755121c453fdd3a
Kazuaki Ishizaki <ishizaki@jp.ibm.com>
2015-10-29 14:07:51 -0400

    add utility to add jcuda native library for bigfat jar

---
+++ gpudev/util/embedjcudalib.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+source args.sh
+cd $DIR
+
+if [ 'uname -s' == "Darwin" ]; then
+  OS=apple
+  ARCH=x86_64
+  EXT=dylib
+else
+  OS=linux
+  ARCH=`arch`
+  EXT=so
+fi
+DISTASM=dist/lib/spark-assembly-1.5.0-GPU-hadoop2.2.0.jar
+
+function addlib2jar {
+  libdir=$2/target
+  lib=lib/$4-${OS}-${ARCH}.${EXT}
+  if [ $1 == "DIST" ]; then
+    jar=${DISTASM}
+  else
+    jar=$1/target/lib/$3.jar
+  fi
+  if [ -e $libdir/$lib ]; then
+    if [ -e $jar ]; then
+      jar uf $jar -C $libdir $lib
+    fi 
+  fi 
+}
+
+for dir in *; do
+  if [ -d $dir ]; then
+     echo $dir is being processed
+     addlib2jar $dir $dir jcuda libJCudaDriver
+     addlib2jar $dir $dir jcuda libJCudaRuntime
+     for lib in ublas ufft urand usolver usparse; do
+       addlib2jar $dir jc${lib} libJC${lib} 
+     done
+  fi
+done
+echo $DISTASM is being processed
+addlib2jar DIST core jcuda libJCudaDriver
+addlib2jar DIST core jcuda libJCudaRuntime
+for lib in ublas ufft urand usolver usparse; do
+  addlib2jar DIST core jc${lib} libJC${lib} 
+done
