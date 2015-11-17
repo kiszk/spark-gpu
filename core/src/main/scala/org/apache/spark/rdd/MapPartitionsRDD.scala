5768eb46e04274d310ec2465fed1aecab13a4273
Kazuaki Ishizaki <ishizaki@jp.ibm.com>
2015-11-17 06:04:46 -0500

    support reduce operation for a rdd with a pritmitive type array
    add guard code using CUDA thread id for reduction in CUDA

--- core/src/main/scala/org/apache/spark/rdd/MapPartitionsRDD.scala
+++ core/src/main/scala/org/apache/spark/rdd/MapPartitionsRDD.scala
@@ -29,7 +29,8 @@ private[spark] class MapPartitionsRDD[U: ClassTag, T: ClassTag](
     prev: RDD[T],
     f: (TaskContext, Int, Iterator[T]) => Iterator[U],  // (TaskContext, partition index, iterator)
     preservesPartitioning: Boolean = false,
-    extfunc: Option[ExternalFunction] = None)
+    extfunc: Option[ExternalFunction] = None,
+    outputArraySizes: Seq[Long] = null)
   extends RDD[U](prev) {
 
   override val partitioner = if (preservesPartitioning) firstParent[T].partitioner else None
@@ -44,7 +45,7 @@ private[spark] class MapPartitionsRDD[U: ClassTag, T: ClassTag](
     (firstParent[T].partitionData(split, context), extfunc) match {
       // computing column-based partition on GPU
       case (col: ColumnPartitionData[T], Some(extfun)) =>
-        extfun.run(col)
+        extfun.run(col, outputArraySizes = outputArraySizes)
 
       // computing  iterator-based partition on CPU
       case (data, _) =>
