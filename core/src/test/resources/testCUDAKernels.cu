8100cfa94b6857d421569060a7b2cbad81db67e1
Kazuaki Ishizaki <ishizaki@jp.ibm.com>
2015-11-19 15:40:26 -0500

    add logistic regression into CUDA testsuite

--- core/src/test/resources/testCUDAKernels.cu
+++ core/src/test/resources/testCUDAKernels.cu
@@ -1,3 +1,5 @@
+#include<math.h>
+
 #define	GET_BLOB_ADDRESS(ptr, offset)	(&((ptr)[(offset)/sizeof((ptr)[0])]))
 #define	GET_ARRAY_CAPACITY(ptr)		(((long *)(ptr))[0])
 #define	GET_ARRAY_LENGTH(ptr)		(((long *)(ptr))[1])
@@ -197,7 +199,7 @@ __global__ void intArraySum(const long *input, const int *inputBlob, long *outpu
 }
 
 // map for DataPoint class
-__global__ void DataPointLRMap(const long *inputX, const int *inputY, const double *inputBlob, long *output, double *outputBlob, long size, const double *inFreeArray) {
+__global__ void DataPointMap(const long *inputX, const int *inputY, const double *inputBlob, long *output, double *outputBlob, long size, const double *inFreeArray) {
     const long ix = threadIdx.x + blockIdx.x * (long)blockDim.x;
     if (ix < size) {
         // copy int array
@@ -219,7 +221,7 @@ __global__ void DataPointLRMap(const long *inputX, const int *inputY, const doub
 }
 
 // reduce for DataPoint class
-__global__ void DataPointLRReduce(const long *input, const double *inputBlob, long *output, double *outputBlob, long size, int stage, int totalStages) {
+__global__ void DataPointReduce(const long *input, const double *inputBlob, long *output, double *outputBlob, long size, int stage, int totalStages) {
     const long ix = threadIdx.x + blockIdx.x * (long)blockDim.x;
     const int jump = 64 * 256;
     if (stage == 0) {
@@ -265,3 +267,125 @@ __global__ void DataPointLRReduce(const long *input, const double *inputBlob, lo
    } 
 }
 
+// map for Logistic regression
+__device__ double sdotvv(const double * __restrict__ x, const double * __restrict__ y, int n) {
+    double ans = 0.0;
+    for(int i = 0; i < n; i++) {
+        ans += x[i] * y[i];
+    }
+    return ans;
+}
+__device__ void dmulvs(double *result, const double * __restrict__ x, double c, int n) {
+    for(int i = 0; i < n; i++) {
+        result[i] = x[i] * c;
+    }
+}
+__device__ void map(double *result, const double * __restrict__ x, double y, const double * __restrict__ w, int n) {
+    dmulvs(result, x, (1 / (1 + exp(-y * (sdotvv(w, x, n)))) - 1) * y, n);
+}
+
+__global__ void LRMap(const long * __restrict__ inputX, const double *  __restrict__ inputY, const double * __restrict__ inputBlob, long *output, double *outputBlob, long size, const double * __restrict__ inputW) {
+    const long ix = threadIdx.x + blockIdx.x * (long)blockDim.x;
+    if (ix < size) {
+        // copy int array
+        long offset = inputX[ix];
+        const double *inArray = GET_BLOB_ADDRESS(inputBlob, offset);
+        const long capacity = GET_ARRAY_CAPACITY(inArray);
+        const long length   = GET_ARRAY_LENGTH(inArray);
+        const double * inArrayBody = GET_ARRAY_BODY(inArray);
+
+        double *outArray = GET_BLOB_ADDRESS(outputBlob, offset);
+        double *outArrayBody = GET_ARRAY_BODY(outArray);
+
+        map(outArrayBody, inArrayBody, inputY[ix], inputW, length);
+
+        output[ix] = offset;
+        SET_ARRAY_CAPACITY(outArray, capacity);
+        SET_ARRAY_LENGTH(outArray, length);
+    }
+}
+
+__device__ inline double *  __shfl_down_double(double *var, unsigned int srcLane, int width=32) {
+    int2 a = *reinterpret_cast<int2*>(&var);
+    a.x = __shfl_down(a.x, srcLane, width);
+    a.y = __shfl_down(a.y, srcLane, width);
+    return *reinterpret_cast<double**>(&a);
+}
+
+__global__ void LRReduce(const long * __restrict__ input, const double * __restrict__ inputBlob, long *output, double *outputBlob, long size/*, int stage, int totalStages*/) {
+    int idx = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if (idx == 0) {
+        double *outArray = GET_BLOB_ADDRESS(outputBlob, input[idx]);
+        double *outArrayBody = GET_ARRAY_BODY(outArray);
+ 
+        long capacity = 0, length = 0;
+        for (long i = 0; i < size; i++) {
+            long offset = input[i];
+            const double *inArray = GET_BLOB_ADDRESS(inputBlob, offset);
+            capacity = GET_ARRAY_CAPACITY(inArray);
+            length   = GET_ARRAY_LENGTH(inArray);
+            const double * __restrict__ inArrayBody = GET_ARRAY_BODY(inArray);
+
+            if (i == 0) {
+                for (long j = 0; j < length; j++) {
+                    outArrayBody[j] = 0;
+                }
+            } 
+
+            for (long j = 0; j < length; j++) {
+                outArrayBody[j] += inArrayBody[j];
+            }
+        }
+        output[idx] = 0;
+        SET_ARRAY_CAPACITY(outArray, capacity);
+        SET_ARRAY_LENGTH(outArray, length);
+    }
+
+#if 0
+    int warpCnt = (threadsPerBlock + warpSize-1)/warpSize;
+    int laneId  = threadIdx.x % warpSize;
+    int warpId  = threadIdx.x / warpSize;
+    double *val = NULL;
+    double *val1 = NULL;
+    // shared memory is allocated per block, not per thread. All threads/warps inside a block share a single instance of variable. 
+    static __shared__ double warpResults[32][D]; // The maximum number of possible warps inside a block is 32 (since max thrdsPerBlock=1024 i.e 32 * 32)
+
+    // All threads inside warp execute synchronously
+    if (idx < size) {
+        const double *accArray = GET_BLOB_ADDRESS(inputBlob, input[idx]);
+        double *accArrayBody = const_cast<double *>GET_ARRAY_BODY(accArray);
+
+        /* FOLD EACH WARP(Set of 32 threds/entries)
+            For each warpSet(32 entries),do the following steps
+            1) add last 16 entries into first 16 
+                If total launched thread is 17, then it would be like 0 + 16, 1+0, 2+0 .. size
+                shufl_down would return zero when there is no thread.
+            2) add second 8 entries into first 8 entries
+            3) and so on... till you add second entry in first entry
+            4) The first thread's val variable in each  warp will contain the sum of all 32 val variable 
+        */
+
+        for (int foldSize = warpSize/2; foldSize; foldSize/=2) {
+            val1 = __shfl_down_double(val, foldSize);
+            if (val1 != NULL)
+                for(int i = 0; i < D; i++) val[i] += val1[i];
+    }
+
+    // Lets sequentially store each warp's sum into array 'a' 
+    if (laneId == 0) {
+        memcpy(warpResults[warpId], val, sizeof(*val) * D);
+    }
+}
+    // Wait for all warps inside a block to complete.
+    __syncthreads();
+
+    if (idx == 0) {
+        for (int w = 0; w < warpCnt;w++)
+            for (int i = 0; i < D;i++)
+                result[i] += warpResults[w][i];
+    }
+
+   //Let each block copy the results into temp result array.
+#endif
+}
