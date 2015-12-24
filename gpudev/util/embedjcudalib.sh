8d50585f31eb014dd98fe998023fa1717e61299c
Kazuaki Ishizaki <ishizaki@jp.ibm.com>
2015-12-23 22:50:59 -0500

    execute jar command only for a directory with target/lib/libJCuda*so
    add command line options to specify parameters

--- gpudev/util/embedjcudalib.sh
+++ gpudev/util/embedjcudalib.sh
@@ -2,6 +2,11 @@
 source args.sh
 cd $DIR
 
+usage() {
+  echo "Usage: $0 [-a ARCH] [-o OS] [-e EXTENSION] [-d HADOOP] [-v VERSION]" 1>&2
+  exit 1
+}
+
 if [ 'uname -s' == "Darwin" ]; then
   OS=apple
   ARCH=x86_64
@@ -11,7 +16,32 @@ else
   ARCH=`arch`
   EXT=so
 fi
-DISTASM=dist/lib/spark-assembly-1.6.0-GPU-hadoop2.4.0.jar
+if [ $ARCH == "ppc64le" ]; then
+  ARCH="ppc_64"
+fi
+VERSION="1.6.0-GPU"
+HADOOP="2.4.0"
+
+while getopts a:o:e:d:v:h OPT
+do
+  case $OPT in
+    a) ARCH=$OPTARG
+       ;;
+    o) OS=$OPTARG
+       ;;
+    e) EXT=$OPTARG
+       ;;
+    d) HADOOP=$OPTARG
+       ;;
+    v) VERSION=$OPTARG
+       ;;
+    h) usage
+       ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+DISTASM=dist/lib/spark-assembly-${VERSION}-hadoop${HADOOP}.jar
 if [ ! -f $DISTASM ]; then
   echo "file $DISTASM does not exist. Please check file name."
   exit 1
@@ -21,6 +51,10 @@ fi
 function addlib2jar {
   libdir=$2/target
   lib=lib/$4-${OS}-${ARCH}.${EXT}
+  if [ ! -f $libdir/$lib ]; then
+    echo "file $libdir/$lib does not exist. Please check file name."
+    exit 1
+  fi
   if [ $1 == "DIST" ]; then
     jar=${DISTASM}
   else
@@ -34,12 +68,12 @@ function addlib2jar {
 }
 
 for dir in *; do
-  if [ -d $dir ]; then
+  if [ -d $dir ] && [ -f $dir/target/lib/libJCudaDrive*.so ]; then
      echo $dir is being processed
      addlib2jar $dir $dir jcuda libJCudaDriver
      addlib2jar $dir $dir jcuda libJCudaRuntime
      for lib in ublas ufft urand usolver usparse; do
-       addlib2jar $dir jc${lib} libJC${lib} 
+       addlib2jar $dir $dir jc${lib} libJC${lib} 
      done
   fi
 done
